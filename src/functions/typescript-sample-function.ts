0b1364c678ed100da9c91d2ce99edbaa11f00a43
danielmb <danbre14@nfk.no>
2024-03-21 14:40:41 +0100

    test

--- src/functions/typescript-sample-function.ts
+++ src/functions/typescript-sample-function.ts
@@ -16,32 +16,8 @@ export async function typescriptSampleFunction(
   return { body: `Hello, ${name}!` };
 }
 
-export async function jobbVennlig(
-  request: HttpRequest,
-  context: InvocationContext,
-): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
-
-  const name = request.query.get('name') || (await request.text());
-  const jobb = request.query.get('jobb') || (await request.text());
-
-  if (!name || !jobb) {
-    context.error('Mangler navn eller jobb');
-    throw new Error('Mangler navn eller jobb');
-  }
-  return {
-    body: `Dette er en jobbvennlig funksjon, ${name}! Din jobb er ${jobb}`,
-  };
-}
-
 app.http('typescript-sample-function', {
   methods: ['GET', 'POST'],
   authLevel: 'anonymous',
   handler: typescriptSampleFunction,
-});
-
-app.http('jobb-vennlig', {
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  handler: jobbVennlig,
 });
